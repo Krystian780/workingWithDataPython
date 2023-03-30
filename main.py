3ec4afa66b963bd063c7b2444202e8f1e179fae9
Krystian MATYSEK (contractor) <krystian.matysek@amadeus.com>
2023-03-30 10:04:22 +0200

    initial_setup

---
+++ main.py
@@ -0,0 +1,5 @@
+from Tracker import Tracker
+from ColumnPrinter import ColumnPrinter
+
+columnPrinter = ColumnPrinter("C:\\Users\\OPEN ACTIVITIES WEMEA RM-2023-03-10-11-30-30.xlsx")
+columnPrinter.printDateColumn()
