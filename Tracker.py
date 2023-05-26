8450da7e39b1c96afc61888df41a86a0dcc22dbe
Krystian MATYSEK (contractor) <krystian.matysek@amadeus.com>
2023-05-26 11:31:25 +0200

    lots_of_changes_to_Date_Occurences_Retriever

--- Tracker.py
+++ Tracker.py
@@ -2,14 +2,18 @@ import pandas as pd
 
 class Tracker:
 
-    def __init__(self, filePath):
-        self.excelFile = pd.read_excel(filePath)
+    def __init__(self, filePath, sheet):
+        self.excelFile = pd.read_excel(filePath, sheet)
 
-    def printDataFrame(self):
+    def printExcel(self):
         print(self.excelFile)
 
     def printHeaders(self):
         print(self.excelFile.head())
+
+    def removeIndex(self):
+        dataframe = self.excelFile
+        self.excelFile = dataframe.reset_index(drop=True)
 
     def dropFirsAndThirdColumn(self):
         self.excelFile.drop(columns=self.excelFile.columns[0],
@@ -24,7 +28,11 @@ class Tracker:
         self.dropFirsAndThirdColumn()
         self.excelFile = dataframe.iloc[16:]
 
-    def getExcel(self):
+    def dropFirstRow(self):
+        dataframe = self.excelFile
+        self.excelFile = dataframe.iloc[1:]
+
+    def getDataFrame(self):
         return self.excelFile
 
     def setFirstRowAsHeader(self):
@@ -38,10 +46,22 @@ class Tracker:
     def getColumnNames(self):
         return self.excelFile.columns.values
 
-    def retrieveColumnWithDate(self):
-        return self.excelFile["Date"]
-
     def printDataType(self):
         print(type(self.excelFile))
 
+    def retrieveFrance(self):
+        return self.excelFile.loc[self.excelFile['Shipping Country'] == 'France']
+    def toExcel(self):
+        self.excelFile.to_excel('C:\\Users\\output.xlsx')
 
+    def retrieveDateColumnFromSalesForce(self):
+        return self.excelFile["Date"]
+
+    def retrieveDateColumnFromFrenchTracker(self):
+        return self.excelFile["Received date"]
+
+    def convertToList(self):
+        return self.excelFile.to_list()
+
+    def printDataType(self):
+        print(self.excelFile.dtypes)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from Tracker import Tracker
-from ColumnPrinter import ColumnPrinter
+from DateOccurencesRetriever import DateOccurencesRetriever
 
-columnPrinter = ColumnPrinter("C:\\Users\\OPEN ACTIVITIES WEMEA RM-2023-03-10-11-30-30.xlsx")
-columnPrinter.printDateColumn()
+dateOccurencesRetriever = DateOccurencesRetriever("C:\\Users\\kmatysek\\OneDrive - Amadeus Workplace\\Desktop\\OPEN ACTIVITIES WEMEA RM-2023-03-10-11-30-30.xlsx", "C:\\Users\\kmatysek\\OneDrive - Amadeus Workplace\\Desktop\\FrenchTracker.xlsx")
+dateOccurencesRetriever.retrieveFrenchDatesFromSalesForce()
+dateOccurencesRetriever.countDateOccurencesFromSalesForce()
+dateOccurencesRetriever.convertListOfStringsToDate("salesForce")
+dateOccurencesRetriever.printEverything()
+print("sharepointtt")
+
